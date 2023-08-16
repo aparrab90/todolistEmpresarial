d66d0a87be3883f33b654e026f4f826a606c4db3
Raquel0701 <diana.diaz7196@gmail.com>
2023-08-16 01:23:38 -0500

    modified Header

--- src/views/general/HeaderView.vue
+++ src/views/general/HeaderView.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
-        <b-navbar variant="primary" type="light" class="fixed-top ">
-            <b-navbar-brand class="d-flex align-items-center justify-content-between w-100 text-light">
-                <!-- Logo a la izquierda -->
-                <a href="/">
-                    <img src="../../assets/logoTodoList2.png" width="70" alt="Logo" class="" />
-                </a>
+        <b-navbar variant="white" type="primary" class="fixed-top shadow">
+            <b-navbar-brand class="d-flex align-items-center justify-content-between w-100 text-primary ">
+                <div style="margin-left: 20px !important;">
+                    <a href="/">
+                        <!-- <img src="../../assets/logoTodoList.png" width="70" alt="Logo" class="" />0 -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            class="bi bi-ui-checks-grid" viewBox="0 0 16 16">
+                            <path
+                                d="M2 10h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1zm9-9h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zm0 9a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-3zm0-10a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2h-3zM2 9a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H2zm7 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2v-3zM0 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.354.854a.5.5 0 1 0-.708-.708L3 3.793l-.646-.647a.5.5 0 1 0-.708.708l1 1a.5.5 0 0 0 .708 0l2-2z" />
+                        </svg>
+                    </a>
+                </div>
 
-                <!-- TodoList centrado -->
                 <h4 class="text-center mx-auto">TodoList Five!</h4>
+
                 <span v-if="isAuthenticated">Hi, {{ nameUser }}</span>
-                <span v-else>Bienvenido, Invitado</span>
+                <span v-else></span>
             </b-navbar-brand>
         </b-navbar>
     </div>
